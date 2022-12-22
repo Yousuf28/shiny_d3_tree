4ae4c8bfe48f00963d2e4b7db2fa1a81c5e1604e
Yousuf Ali <yousuf.pharma@gmail.com>
2022-12-22 10:00:28 -0500

    organize file

--- get_tree_back.js
+++ get_tree_back.js
@@ -1,22 +1,115 @@
 
-// let tree = tree_root.children;
-var all_nodes = [];
+var tree_obj = {
+    "name": "Benefit_Risk",
+ 
+        "children": [{
+            "name": "Benefits",
+            "children": [{
+                "name": "Benefit category 1",
+				"children":[{
+					"name": "Primary efficacy outcome"
+				},
+				{"name": "Secondary efficacy outcome"}
+			]
+            }, {
+                "name": "Benefit category 2",
+				"children":[{
+					"name": "Tertiary efficacy outcome"
+				}]
+            }
+		]
+        }, {
+            "name": "Risks",
+            "children": [{
+                "name": "Risk category 1",
+				"children":[
+					{
+						"name": "principal safety outcome"
+					}
+				]
+            }, {
+                "name": "Risk category 2",
+				"children": [
+					{
+						"name": "Adverse event 1"
+					},
+					{
+						"name": "Adverse event 2"
+					},
+					{
+						"name": "Adverse event 3"
+					}
+				]
+            }
+		]
+        }]
+    
+}
+
+
+
+// var all_nodes = [];
+// function getName(tree_root){
+//     console.log(tree_root.name)
+//     all_nodes.push(tree_root.name)
+    
+//     if(tree_root.children){
+
+//         let tree = tree_root.children;
+//         // debugger
+//           let count = tree.length;
+          
+//           if(count){
+      
+//           for (let i = 0; i < count; i++) {
+//               const element = tree[i];
+              
+              
+//                 //   console.log(element.name, element.id)
+//                 //   all_nodes.push(element.name)
+//                   getName(element)
+      
+              
+              
+      
+              
+//           }
+//       }
+        
+//     }
+// }
+
+var all_nodes = {
+"name": "",
+"children": []
+
+};
+
+var count_iter = 0;
 function getName(tree_root){
+    console.log(tree_root.name)
+    
     
     if(tree_root.children){
+        
 
         let tree = tree_root.children;
         // debugger
           let count = tree.length;
+          console.log(count)
           
           if(count){
+           
       
           for (let i = 0; i < count; i++) {
               const element = tree[i];
               
               
-                  console.log(element.name)
-                  all_nodes.push(element.name)
+                //   console.log(element.name, element.id)
+                //   all_nodes.push(element.name)
+                // all_nodes.children[i] = {"name": element.name};
+                count_iter++
+                // console.log(count_iter)
                   getName(element)
       
               
@@ -27,11 +120,10 @@ function getName(tree_root){
       }
         
     }
-
-   
-
-
 }
+getName(tree_json)
+
+console.log(all_nodes)
 
 
 
@@ -47,3 +139,50 @@ function getName(tree_root){
 
 // }
 // )
+
+var tree_obj = {
+    "name": "Benefit_Risk",
+ 
+        "children": [{
+            "name": "Benefits",
+            "children": [{
+                "name": "Benefit category 1",
+				"children":[{
+					"name": "Primary efficacy outcome"
+				},
+				{"name": "Secondary efficacy outcome"}
+			]
+            }, {
+                "name": "Benefit category 2",
+				"children":[{
+					"name": "Tertiary efficacy outcome"
+				}]
+            }
+		]
+        }, {
+            "name": "Risks",
+            "children": [{
+                "name": "Risk category 1",
+				"children":[
+					{
+						"name": "principal safety outcome"
+					}
+				]
+            }, {
+                "name": "Risk category 2",
+				"children": [
+					{
+						"name": "Adverse event 1"
+					},
+					{
+						"name": "Adverse event 2"
+					},
+					{
+						"name": "Adverse event 3"
+					}
+				]
+            }
+		]
+        }]
+    
+}
